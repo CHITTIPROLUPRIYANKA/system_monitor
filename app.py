6da67ac58ea0baadc32a61485f603939751b6949
CHITTIPROLUPRIYANKA <chp3096@gmail.com>
2026-03-28 20:48:25 +0530

    fix api delay

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import StringIO
 app = Flask(__name__)
 
 def get_system_data():
-    cpu = psutil.cpu_percent(interval=1)
+    cpu = psutil.cpu_percent(interval=None)
     ram = psutil.virtual_memory().percent
     disk = psutil.disk_usage('C:\\').percent
 
